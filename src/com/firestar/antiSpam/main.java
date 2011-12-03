673d051131fa220d3246eb4451fe6066c66fcf7b
Sean Gordon <sean_aldon@hotmail.co.uk>
2011-12-03 03:07:09 +0000

    Prettified

--- src/com/firestar/antiSpam/main.java
+++ src/com/firestar/antiSpam/main.java
@@ -16,65 +16,73 @@ import com.nijiko.permissions.PermissionHandler;
 import com.nijikokun.bukkit.Permissions.Permissions;
 
 public class main extends JavaPlugin {
+
 	private PermissionHandler Permissions = null;
-	static private Logger log = Logger.getLogger("Minecraft");
+	private static final Logger log = Logger.getLogger("Minecraft");
 	private HashMap<String, Boolean> actionTaken = new HashMap<String, Boolean>();
 	private chat playerListener = null;
+
 	public void onDisable() {
-		Message("Disabled antiSpam");
+		message("Disabled antiSpam");
 	}
+
 	public void onEnable() {
-		Message("Enabled antiSpam");
-        File fileSettings = new File("plugins/antiSpam/settings.yml");
-        Configuration main_config = null;
-        if(fileSettings.exists()){
-        	Message("Configuration File Found!");
-        	main_config = new Configuration(fileSettings);
-            main_config.load();
-            playerListener = new chat(this,Integer.valueOf(main_config.getString("maxMessage")),Integer.valueOf(main_config.getString("maxTime")));
-        }else{
-        	Message("No Configuration Detected, Default Settings!");
-        	playerListener = new chat(this,4,2);
-        }
+		message("Enabled antiSpam");
+		File fileSettings = new File("plugins/antiSpam/settings.yml");
+		Configuration main_config = null;
+		if (fileSettings.exists()) {
+			message("Configuration File Found!");
+			main_config = new Configuration(fileSettings);
+			main_config.load();
+			playerListener = new chat(this, Integer.valueOf(main_config.getString("maxMessage")), Integer.valueOf(main_config.getString("maxTime")));
+		} else {
+			message("No Configuration Detected, Default Settings!");
+			playerListener = new chat(this, 4, 2);
+		}
 		PluginManager pm = getServer().getPluginManager();
 		pm.registerEvent(Event.Type.PLAYER_CHAT, playerListener, Priority.Highest, this);
 		setupPermissions();
 	}
+
 	public void setupPermissions() {
 		Plugin test = getServer().getPluginManager().getPlugin("Permissions");
-		if(Permissions == null) {
-		    if(test != null) {
-		    	Permissions = ((Permissions)test).getHandler();
-		    	Message("Found Permission Bridge!");
-		    } else {
-		    }
+		if (Permissions == null) {
+			if (test != null) {
+				Permissions = ((Permissions) test).getHandler();
+				message("Found Permission Bridge!");
+			} else {
+			}
 		}
 	}
-	public boolean actionGet(String player){
-		if(actionTaken.containsKey(player)){
+
+	public boolean getAction(String player) {
+		if (actionTaken.containsKey(player)) {
 			return actionTaken.containsKey(player);
 		}
 		return false;
 	}
-	public void actionSet(String player, Boolean set){
-		actionTaken.put(player,set);
+
+	public void setAction(String player, Boolean set) {
+		actionTaken.put(player, set);
 	}
-	public boolean hasPerm(Player player){
-		if(Permissions==null){
-			if(player.hasPermission("antiSpam")){
+
+	public boolean hasPerm(Player player) {
+		if (Permissions == null) {
+			if (player.hasPermission("antiSpam")) {
 				return true;
-			}else{
+			} else {
 				return false;
 			}
-		}else{
-			if(Permissions.has(player, "antiSpam")){
+		} else {
+			if (Permissions.has(player, "antiSpam")) {
 				return true;
-			}else{
+			} else {
 				return false;
 			}
 		}
 	}
-	public void Message(String msg){
-		log.info("antiSpam: "+msg);
+
+	public void message(String msg) {
+		log.info("antiSpam: " + msg);
 	}
 }
