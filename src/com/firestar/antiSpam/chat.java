7353732888581254290bcf17c885f852c1b36a8d
ryanclancy000 <ryanclancy000@yahoo.ca>
2012-03-04 20:56:39 -0400

    Added support for new event system (R6+ and 1.2 compatible) - Added config file, rather than settings.yml

--- src/com/firestar/antiSpam/chat.java
+++ src/com/firestar/antiSpam/chat.java
@@ -2,70 +2,74 @@ package com.firestar.antiSpam;
 
 import com.mcbans.firestar.mcbans.BukkitInterface;
 import com.mcbans.firestar.mcbans.pluginInterface.Ban;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.HashMap;
+import org.bukkit.Server;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
 
-public class chat implements Listener {
+public class chat
+  implements Listener
+{
+  private HashMap<String, ArrayList<Long>> chatLastSent = new HashMap();
+  private int maxMSG = 5;
+  private int maxTM = 4;
+  private main plugin = null;
+  private BukkitInterface mcb = null;
 
-    private HashMap<String, ArrayList<Long>> chatLastSent = new HashMap<String, ArrayList<Long>>();
-    private int maxMSG = 5;
-    private int maxTM = 4;
-    private main plugin = null;
-    private BukkitInterface mcb = null;
+  public chat(main d, int messageC, int maxTime) {
+    this.maxMSG = messageC;
+    this.maxTM = (maxTime * 1000);
+    this.plugin = d;
+    setupMCBans();
+  }
 
-    public chat(main d, int messageC, int maxTime) {
-        maxMSG = messageC;
-        maxTM = maxTime * 1000;
-        plugin = d;
-        setupMCBans();
+  public void setupMCBans() {
+    Plugin test = this.plugin.getServer().getPluginManager().getPlugin("mcbans");
+    if ((this.mcb == null) && 
+      (test != null)) {
+      this.mcb = ((BukkitInterface)test);
+      this.plugin.message("Found mcbans, enabling global ban!");
     }
+  }
 
-    public void setupMCBans() {
-        Plugin test = plugin.getServer().getPluginManager().getPlugin("mcbans");
-        if (mcb == null) {
-            if (test != null) {
-                mcb = ((BukkitInterface) test);
-                plugin.message("Found mcbans, enabling global ban!");
-            }
+  @EventHandler
+  public void onPlayerChat(PlayerChatEvent event) {
+    long timeInMillis = System.currentTimeMillis();
+    if (!this.plugin.hasPerm(event.getPlayer()))
+      if (this.chatLastSent.containsKey(event.getPlayer().getName())) {
+        ArrayList g = new ArrayList();
+        int tmpderp = 1;
+        for (Long tmp : (ArrayList)this.chatLastSent.get(event.getPlayer().getName())) {
+          if (this.maxTM + tmp.longValue() > timeInMillis) {
+            tmpderp++;
+            g.add(tmp);
+          }
         }
-    }
-
-    @EventHandler
-    public void onPlayerChat(PlayerChatEvent event) {
-        long timeInMillis = System.currentTimeMillis();
-        if (!plugin.hasPerm(event.getPlayer())) {
-            if (chatLastSent.containsKey(event.getPlayer().getName())) {
-                ArrayList<Long> g = new ArrayList<>();
-                int tmpderp = 1;
-                for (Long tmp : chatLastSent.get(event.getPlayer().getName())) {
-                    if ((maxTM + tmp) > timeInMillis) {
-                        tmpderp++;
-                        g.add(tmp);
-                    }
-                }
-                if (tmpderp >= maxMSG) {
-                    if (!plugin.getAction(event.getPlayer().getName())) {
-                        if (mcb != null) {
-                            Ban ban = new Ban(mcb, "globalBan", event.getPlayer().getName(), event.getPlayer().getAddress().getAddress().getHostAddress(), "[antiSpam]", "spambot", "", "");
-                            ban.start();
-                        } else {
-                            event.getPlayer().kickPlayer("Stop spamming!");
-                        }
-                        plugin.setAction(event.getPlayer().getName(), true);
-                    }
-                    event.setCancelled(true);
-                }
-                g.add(timeInMillis);
-                chatLastSent.put(event.getPlayer().getName(), g);
+        if (tmpderp >= this.maxMSG) {
+          if (!this.plugin.getAction(event.getPlayer().getName())) {
+            if (this.mcb != null) {
+              Ban ban = new Ban(this.mcb, "globalBan", event.getPlayer().getName(), event.getPlayer().getAddress().getAddress().getHostAddress(), "[antiSpam]", "spambot", "", "");
+              ban.start();
             } else {
-                ArrayList<Long> g = new ArrayList<>();
-                g.add(timeInMillis);
-                chatLastSent.put(event.getPlayer().getName(), g);
+              event.getPlayer().kickPlayer("Stop spamming!");
             }
+            this.plugin.setAction(event.getPlayer().getName(), Boolean.valueOf(true));
+          }
+          event.setCancelled(true);
         }
-    }
+        g.add(Long.valueOf(timeInMillis));
+        this.chatLastSent.put(event.getPlayer().getName(), g);
+      } else {
+        ArrayList g = new ArrayList();
+        g.add(Long.valueOf(timeInMillis));
+        this.chatLastSent.put(event.getPlayer().getName(), g);
+      }
+  }
 }
--- src/com/firestar/antiSpam/main.java
+++ src/com/firestar/antiSpam/main.java
@@ -4,71 +4,67 @@ import com.nijiko.permissions.PermissionHandler;
 import com.nijikokun.bukkit.Permissions.Permissions;
 import java.util.HashMap;
 import java.util.logging.Logger;
+import org.bukkit.Server;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.FileConfigurationOptions;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class main extends JavaPlugin {
+public class main extends JavaPlugin
+{
+  private PermissionHandler Permissions = null;
+  private static final Logger log = Logger.getLogger("Minecraft");
+  private HashMap<String, Boolean> actionTaken = new HashMap();
+  private chat playerListener = null;
 
-    private PermissionHandler Permissions = null;
-    private static final Logger log = Logger.getLogger("Minecraft");
-    private HashMap<String, Boolean> actionTaken = new HashMap<>();
-    private chat playerListener = null;
+  public void onDisable()
+  {
+  }
 
-    @Override
-    public void onDisable() {
+  public void onEnable()
+  {
+    getConfig().options().copyDefaults(true);
+    this.playerListener = new chat(this, Integer.valueOf(getConfig().getString("maxMessage")).intValue(), Integer.valueOf(getConfig().getString("maxTime")).intValue());
+    this.playerListener = new chat(this, 5, 2);
+    PluginManager pm = getServer().getPluginManager();
+    getServer().getPluginManager().registerEvents(this.playerListener, this);
+    saveConfig();
+  }
+
+  public void setupPermissions() {
+    Plugin test = getServer().getPluginManager().getPlugin("Permissions");
+    if ((this.Permissions == null) && 
+      (test != null)) {
+      this.Permissions = ((Permissions)test).getHandler();
+      message("Found Permission Bridge!");
+    }
+  }
+
+  public boolean getAction(String player)
+  {
+    if (this.actionTaken.containsKey(player)) {
+      return this.actionTaken.containsKey(player);
+    }
+    return false;
+  }
+
+  public void setAction(String player, Boolean set) {
+    this.actionTaken.put(player, set);
+  }
+
+  public boolean hasPerm(Player player) {
+    if (this.Permissions == null)
+    {
+      return player.hasPermission("antiSpam");
     }
 
-    @Override
-    public void onEnable() {
-        getConfig().options().copyDefaults(true);
-        playerListener = new chat(this, Integer.valueOf(getConfig().getString("maxMessage")), Integer.valueOf(getConfig().getString("maxTime")));
-        playerListener = new chat(this, 5, 2);
-        PluginManager pm = getServer().getPluginManager();
-        getServer().getPluginManager().registerEvents(playerListener, this);
-        saveConfig();
-    }
+    return this.Permissions.has(player, "antiSpam");
+  }
 
-    public void setupPermissions() {
-        Plugin test = getServer().getPluginManager().getPlugin("Permissions");
-        if (Permissions == null) {
-            if (test != null) {
-                Permissions = ((Permissions) test).getHandler();
-                message("Found Permission Bridge!");
-            } else {
-            }
-        }
-    }
-
-    public boolean getAction(String player) {
-        if (actionTaken.containsKey(player)) {
-            return actionTaken.containsKey(player);
-        }
-        return false;
-    }
-
-    public void setAction(String player, Boolean set) {
-        actionTaken.put(player, set);
-    }
-
-    public boolean hasPerm(Player player) {
-        if (Permissions == null) {
-            if (player.hasPermission("antiSpam")) {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            if (Permissions.has(player, "antiSpam")) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }
-
-    public void message(String msg) {
-        log.info("antiSpam: " + msg);
-    }
+  public void message(String msg)
+  {
+    log.info("antiSpam: " + msg);
+  }
 }
